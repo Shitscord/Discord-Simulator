47ab226891ac3220fa5accccda77f58d02cc6670
Sagan <saganm316@gmail.com>
2018-11-06 18:45:53 -0500

    fixed logger to avoid recording  single periods and bots own messages

--- logger.py
+++ logger.py
@@ -1,36 +1,42 @@
 import asyncio, time, os, errno
 
 def log(message):
+    
+    blocked_text = [" .",".","$simulate"]
+    
     async def asyncscript(message):
-        messagestr = " "+str(message.content)
-        messageser = str(message.server.id)
-        messagecha = str(message.channel.id)
-
-        if not messagestr.endswith(".") and not messagestr.endswith("!") and not messagestr.endswith("?"):
-            messagestr = messagestr+"."
-
-        print("Message: ", messagestr)
-        print("Server: ", messageser)
-        print("Channel: ", messagecha)
-
-        if messagestr == " ." or messagestr == ".":
+        if str(message.author)=="DiscordSimulator#3905" or str(message.author)=="Discord Simulator Dev#4642":
             pass
         else:
-        
-            try:
-                os.makedirs("data/"+messageser)
-            except OSError as e:
-                if e.errno != errno.EEXIST:
-                    raise       
-            filename = "data/"+messageser+"/"+messagecha+".txt"
-            time.sleep(.05)
-            if os.path.exists(filename):
-                append_write = 'a'
+            messagestr = " "+str(message.content)
+            messageser = str(message.server.id)
+            messagecha = str(message.channel.id)
+    
+            if not messagestr.endswith(".") and not messagestr.endswith("!") and not messagestr.endswith("?"):
+                messagestr = messagestr+"."
+    
+            print("Message: ", messagestr)
+            print("Server: ", messageser)
+            print("Channel: ", messagecha)
+    
+            if messagestr.lower() in blocked_text:
+                pass
             else:
-                append_write = 'w'
             
-            corpus = open(filename,append_write)
-            corpus.write(messagestr)
-            corpus.close()                  
+                try:
+                    os.makedirs("data/"+messageser)
+                except OSError as e:
+                    if e.errno != errno.EEXIST:
+                        raise       
+                filename = "data/"+messageser+"/"+messagecha+".txt"
+                time.sleep(.05)
+                if os.path.exists(filename):
+                    append_write = 'a'
+                else:
+                    append_write = 'w'
+                
+                corpus = open(filename,append_write)
+                corpus.write(messagestr)
+                corpus.close()                  
 
     asyncio.ensure_future(asyncscript(message))
--- markover.py
+++ markover.py
@@ -1,37 +1,30 @@
 import constants, asyncio, markovify, time
 
 def mscript(command, client, message):
-    mlist=[]
 
-    
     async def asyncscript(command,client,message):
-        async def get_logs_from(channel):
-            async for m in client.logs_from(channel):
-                mstr=str(m.clean_content)
-                mauthor=str(m.author)
-                if mauthor=="DiscordSimulator#3905" or mauthor=="Discord Simulator Dev#4642":
-                    pass
-                else:
-                    if mstr.endswith("."):
-                        mlist.append(mstr)
-                    else:
-                        mstr=mstr+"."
-                        mlist.append(mstr)
-                        
-                        
-        await get_logs_from(message.channel)
-        mfullstr=str(" ".join(str(s) for s in mlist))
+        await client.send_typing(message.channel)
+        messageser = str(message.server.id)
+        messagecha = str(message.channel.id)
+        corpusFile = "data/"+messageser+"/"+messagecha+".txt"
+        corpusRead = open(corpusFile, "r")
+        corpusText = corpusRead.read()
+        print(corpusText)
         
-        x=True
-        while x:
-            final_gen=markovify.Text(mfullstr)
-            final_sent=final_gen.make_sentence()
-            time.sleep(.0025)
-            if "None" not in str(final_sent):
-                print(final_sent)
-                x=False
-            else:
-                print("None")          
-        constants.run_coro(client.send_message(message.channel, str(final_sent)), client)
-    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     asyncio.ensure_future(asyncscript(command, client, message))
