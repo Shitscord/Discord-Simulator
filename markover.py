e642d59e081bd0d982c2edab4fde32973a1355c0
Sagan <saganm316@gmail.com>
2018-12-11 17:43:11 -0500

    Added extra json integration, add json files to json directory and adjust random.randint in markovify

--- markover.py
+++ markover.py
@@ -1,16 +1,27 @@
-import constants, asyncio, markovify
+import constants, asyncio, markovify, random, json
 
 def mscript(command, client, message):
 
     async def asyncscript(command,client,message):
         await client.send_typing(message.channel)
+        
+        #Randomly select 1 of 48 pieces of wikipedia corpus to include for extra entropy
+        wikinum=random.randint(1,1500)
+        wikicorpus="json/"+str(wikinum)+".txt.json"
+        print(wikicorpus)
+        with open(wikicorpus) as jsonFile:
+            wikicorpjson=json.load(jsonFile)
+        wikiimport=markovify.Text.from_json(wikicorpjson)
+        
+        #Get message channel and server and read/analyze contents of log
         messageser = str(message.server.id)
         messagecha = str(message.channel.id)
-        corpusFile = "data/"+messageser+"/"+messagecha+".txt"
-        corpusRead = open(corpusFile, "r",encoding='utf-8')
-        corpusText = corpusRead.read()
+        discordText = open("data/"+messageser+"/"+messagecha+".txt", "r",encoding='utf-8').read()
+        discordChain = markovify.Text(discordText, state_size=3)
         
-        corpusModel = markovify.Text(corpusText)
+        
+        
+        corpusModel = markovify.combine([discordChain, wikiimport], [3,.25])
         
         markovString=""
         for i in range(3):
