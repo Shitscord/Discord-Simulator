aa59830e9cd51e3431a764dd1076e9877720dc4e
thelonewolf316 <thelonewolf316>
2019-02-07 16:58:02 -0500

    disabled file loading for now

--- markover.py
+++ markover.py
@@ -5,6 +5,7 @@ def mscript(command, client, message):
     async def asyncscript(command,client,message):
         await client.send_typing(message.channel)
         
+        '''
         #Randomly select 1 of 48 pieces of wikipedia corpus to include for extra entropy
         wikinum=random.randint(1,1500)
         wikicorpus="json/"+str(wikinum)+".txt.json"
@@ -12,20 +13,20 @@ def mscript(command, client, message):
         with open(wikicorpus) as jsonFile:
             wikicorpjson=json.load(jsonFile)
         wikiimport=markovify.Text.from_json(wikicorpjson)
-        
+        '''
+
         #Get message channel and server and read/analyze contents of log
         messageser = str(message.server.id)
         messagecha = str(message.channel.id)
         discordText = open("data/"+messageser+"/"+messagecha+".txt", "r",encoding='utf-8').read()
-        discordChain = markovify.Text(discordText, state_size=3)
+        discordChain = markovify.Text(discordText)
         
         
-        
-        corpusModel = markovify.combine([discordChain, wikiimport], [3,.25])
+        #corpusModel = markovify.combine([discordChain, wikiimport], [3,.25])
         
         markovString=""
         for i in range(3):
-            markovString += str(corpusModel.make_short_sentence(140))
+            markovString += str(discordChain.make_short_sentence(140))
             markovString += " "
             
         constants.run_coro(client.send_message(message.channel, markovString), client)
